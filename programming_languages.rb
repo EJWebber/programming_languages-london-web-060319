9e35e671dc0a60241f0d1b61ba117365d2c5bad1
Ed Webber <edwebber06@gmail.com>
2019-05-28 17:44:06 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,8 @@
 def reformat_languages(languages)
   # your code here
+  new_languages = {}
+  language_all = languages[:oo].values.merge(languages[:functional].values)
+  language_all.each |value|
+  new_languages[value]={}
+end
 end
